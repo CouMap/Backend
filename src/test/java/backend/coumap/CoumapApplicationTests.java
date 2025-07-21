1a0e18b2e9c461030074704a6608144d7e3cc46e
seoyeong-y <ysy2329@gmail.com>
2025-07-21 19:44:57 +0900

    Chore: 빌드 설정

--- src/test/java/backend/coumap/CoumapApplicationTests.java
+++ src/test/java/backend/coumap/CoumapApplicationTests.java
@@ -2,12 +2,12 @@ package backend.coumap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // 추가
 class CoumapApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
